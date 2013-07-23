33a291e43add645b2b8a4cba9871c6efd174bfb9
Vidit Ochani <vidit.ochani@gmail.com>
2013-07-23 05:26:08 -0700

    Fix install script, and add aliases to git

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
 BACKUP_DIR="$HOME/.backup"
-[[ -d "BACKUP_DIR" ]] && mkdir "$BACKUP_DIR"
+[[ ! -d "BACKUP_DIR" ]] && mkdir "$BACKUP_DIR"
 for name in *; do
   target="$HOME/.$name"
   if [ -e "$target" ]; then
-    echo "WARNING: $target exists, copying to $BACKUP_DIR."
-    cp -r "$target" "$BACKUP_DIR/$target"
+    echo "WARNING: $target exists, moving to $BACKUP_DIR"
+    mv "$target" "$BACKUP_DIR/.$name"
   fi
   if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ]; then
     echo "Creating $target"
     ln -s "$PWD/$name" "$target"
   fi
+  echo "$name";
 done
 
 command -v git >/dev/null 2>&1 || ({ echo "Git required for plugins"; exit; })
